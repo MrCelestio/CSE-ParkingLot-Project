d0fa413136df8426a70e06f81b33d9d224767807
Eric B <ebb618@gmail.com>
2017-10-11 01:13:06 -0400

    Finished parkinglot features (Pre GUI)

---
+++ ParkingLotProject/src/gui/Main.java
@@ -0,0 +1,19 @@
+package gui;
+
+import javafx.application.*;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
